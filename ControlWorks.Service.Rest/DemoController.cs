0941d0c02a2085359b7c883d9fb69ded8cdd9ad9
Bill Heine <whheine@gmail.com.net>
2016-09-20 17:35:14 -0500

    Added Rest Services

---
+++ ControlWorks.Service.Rest/DemoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ControlWorks.Service.Rest
+{
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Greeting()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
--- ControlWorks.Services.Faspac/Program.cs
+++ ControlWorks.Services.Faspac/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
-namespace ControlWorks.Services.Faspac
+namespace ControlWorks.Service.Faspac
 {
     class Program
     {
         static void Main(string[] args)
         {
+            const string name = "ControlWorksFaspacService";
+            const string description = "Control Works communication service";
+
+            try
+            {
+                var host = HostFactory.New(configuration =>
+                {
+                    configuration.Service<Host>(callback =>
+                    {
+                        callback.ConstructUsing(s => new Host());
+                        callback.WhenStarted(service => service.Start());
+                        callback.WhenStopped(service => service.Stop());
+                    });
+                    configuration.SetDisplayName(name);
+                    configuration.SetServiceName(name);
+                    configuration.SetDescription(description);
+                    configuration.RunAsLocalService();
+                });
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("WcfCommunicationService Service fatal exception. " + ex.Message);
+            }
+
         }
     }
 }
