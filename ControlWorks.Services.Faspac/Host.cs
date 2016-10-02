640ee8db993fdbf93a242c4be4d7c882d1410f08
Bill Heine <whheine@gmail.com.net>
2016-10-02 16:58:29 -0500

    Update

---
+++ ControlWorks.Services.Faspac/Host.cs
@@ -0,0 +1,33 @@
+﻿using ControlWorks.Logging;
+using ControlWorks.Service.Rest;
+using ControlWorks.Services.Configuration;
+using log4net;
+using System.Threading.Tasks;
+
+namespace ControlWorks.Service.Faspac
+{
+    public class Host
+    {
+        protected ILogger Logger { get; }
+
+        public Host() { }
+
+        public Host(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Start()
+        {
+            Logger.Log(new LogEntry(LoggingEventType.Information, "Host: Start"));
+
+            Startup.Start(Logger);
+        }
+
+        public void Stop()
+        {
+            Logger.Log(new LogEntry(LoggingEventType.Information, "Host: Stop"));
+
+        }
+    }
+}
